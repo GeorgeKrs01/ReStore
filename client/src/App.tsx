721fd2c6202d61d40a58c070820ed6251cf9a7ae
GeorgeKrs01 <georgekrs@protonmail.com>
2023-10-06 14:30:48 +0300

    Adding Objs

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,10 @@
 import { useEffect, useState } from "react";
+import { Product } from "./product";
 
 
 
 function App() {
-  const [products, setProducts] = useState(
-    [
-      { name: '1', price: "10" },
-      { name: '2', price: "20" }
-    ]);
+  const [products, setProducts] = useState<Product[]>([]);
 
   useEffect(() => {
     fetch('http://localhost:5131/api/products')
@@ -17,15 +14,24 @@ function App() {
 
 
   function addProduct() {
-    setProducts(prevState => [...prevState, { name: 'product3' + (prevState.length + 1), price: `${(prevState.length * 100) + 100}` }])
+    setProducts(prevState => [...prevState,
+    {
+      id: prevState.length + 101,
+      name: 'product3' + (prevState.length + 1),
+      price: (prevState.length * 100) + 100,
+      brand: 'sadas',
+      description: 'aiosufhsaiu hasidu h',
+      pictureUrl: 'http://picsum.photos/200',
+    }
+    ])
   }
 
   return (
     <div className="App">
       <h1>Re-Store</h1>
       <ul>
-        {products.map((item, index) => (
-          <li key={index}>{item.name} - {item.price}</li>
+        {products.map(product => (
+          <li key={product.id}>{product.name} - {product.price}</li>
         ))}
       </ul>
       <button onClick={addProduct}>Add Product</button>
